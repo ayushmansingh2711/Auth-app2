8c4425a4e8b09924c5ef94844328684a97a4e22d
ayushmansingh2711 <ayushmansingh9161@gmail.com>
2025-04-23 19:01:05 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -52,6 +52,6 @@ sequelize.authenticate()
 // Routes
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
